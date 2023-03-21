584e27b92b226d1b8649c2db6a550f2f51d81d3e
Mazilescu Eduard <edymazilescu@gmail.com>
2023-03-21 14:00:29 +0200

    First commit

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 
 def current_path():
-    # print("Current working directory is:", os.getcwd())
+    print("Current working directory is:", os.getcwd())
     print()
 
 
@@ -48,81 +48,34 @@ def folder_synchronization(source_folder, replica_folder, log_file):
                 shutil.copy2(source_path, replica_path)
                 log(log_file, f"Copied {name}")
             my_file.close()
+        for dir in directories:
+            subdirectory = os.path.join(source_folder, dir)
+            replica_subdir = os.path.join(replica_folder, dir)
+            if not os.path.exists(replica_subdir):
+                dir_path = os.path.join(replica_folder, dir)
+                os.mkdir(dir_path)
+                log(log_file, f"Creating {dir} directory")
+            folder_synchronization(subdirectory, replica_subdir, log_file)
 
 
-        for i in directories:
-            #dir_path = os.path.join(root, i)
-            #source_dir_path = os.path.join(source_folder, dir_path)
-            #replica_dir = os.path.join(replica_folder, dir_path)
-            dir_path = os.path.join(root, i)
+def removing(source_folder, replica_folder, log_file):
+    change_directory(replica_folder)
+    for (root, directories, files) in os.walk('.', topdown=True):
+        for name in files:
+            replica_path = os.path.join(root, name)
+            relative_path = os.path.relpath(replica_path, replica_folder)
+            source_path = os.path.join(source_folder, relative_path)
+
+            if not os.path.exists(source_path):
+                os.remove(replica_path)
+                log(log_file, f"File not found in source, deleting {name}")
+        for dir in directories:
+            dir_path = os.path.join(root, dir)
             source_dir_path = os.path.join(source_folder, dir_path)
-            replica_dir = os.path.join(replica_folder, dir_path)
-            print(dir_path)
-            if not os.path.exists(replica_dir):
-                if root!=".":
-                    replicadir=os.path.join(replica_folder,i)
-                    finalreplicadir=os.path.join(replicadir,root)
-                    print(finalreplicadir)
-                    #os.mkdir(finalreplicadir)
-                else:
-                    print(root)
-                    print(dir_path)
-                    print(i)
-                    dirpath=os.path.join(replica_folder, i)
-                    os.mkdir(dirpath)
-                    log(log_file, f"Directory does not exist, creating {i}")
-            if os.path.exists(source_dir_path):
-                change_directory(source_dir_path)
-                current_path()
-                for (root, directories, files) in os.walk('.', topdown=True):
-                    for name in files:
-                        source_path = os.path.join(dir_path, name)
-                        absolute_path = os.path.join(source_folder, source_path)
-                        replica_path = os.path.join(replica_folder, source_path)
-
-                        my_file = open(absolute_path, 'rb')
-                        md5_source = hashlib.md5(my_file.read()).hexdigest()
-
-                        if os.path.exists(replica_path):
-                            my_file = open(replica_path, 'rb')
-                            md5_replica = hashlib.md5(my_file.read()).hexdigest()
-                        else:
-                            md5_replica = None
-                        if md5_replica != md5_source:
-                            shutil.copy2(absolute_path, replica_path)
-                            log(log_file, f"Copying {name} from {i}")
-                        my_file.close()
-
-        # deleting from replica
-        change_directory(replica_folder)
-        for (root, directories, files) in os.walk('.', topdown=True):
-            for name in files:
-                replica_path = os.path.join(root, name)
-                relative_path = os.path.relpath(replica_path, replica_folder)
-                source_path = os.path.join(source_folder, relative_path)
-
-                if not os.path.exists(source_path):
-                    print(replica_path)
-                    os.remove(replica_path)
-
-            for i in directories:
-                dir_path = os.path.join(root, i)
-                source_dir_path = os.path.join(source_folder, dir_path)
-                replica_dir = os.path.join(replica_folder, dir_path)
-                if not os.path.exists(source_dir_path):
-                    shutil.rmtree(dir_path)
-                    log(log_file, f"Deleting {i}")
-                if os.path.exists(source_dir_path):
-                    change_directory(replica_dir)
-                    for (root, directories, files) in os.walk('.', topdown=True):
-                        for name in files:
-                            replica_path = os.path.join(dir_path, name)
-                            replica_path_absolute = os.path.join(replica_dir, name)
-                            relative_path = os.path.relpath(replica_path, replica_folder)
-                            source_path = os.path.join(source_folder, replica_path)
-                            if not os.path.exists(source_path):
-                                os.remove(replica_path_absolute)
-                                log(log_file, f"Deleting {name} from {replica_dir}")
+            replica_subdir = os.path.join(replica_folder, dir_path)
+            if not os.path.exists(source_dir_path):
+                shutil.rmtree(replica_subdir)
+                log(log_file, f"Directory not found in source, deleting {dir}")
 
 
 def log(log_path, message):
@@ -136,4 +89,5 @@ def log(log_path, message):
 if __name__ == '__main__':
     while True:
         folder_synchronization(args.source, args.replica, args.log_file)
+        removing(args.source, args.replica, args.log_file)
         time.sleep(args.interval)
